7be8b2cc1009fd1532354e167eeff1fe9345ff4b
Eneko Uruñuela <e.urunuela@icloud.com>
2022-06-01 16:03:27 -0400

    Added allow_pickle=True

--- pySPFM/tests/test_fista.py
+++ pySPFM/tests/test_fista.py
@@ -5,12 +5,12 @@ from pySPFM.deconvolution import fista
 
 def test_fista_pylops(sim_data, sim_hrf, pylops_results):
 
-    y = np.load(sim_data)
-    hrf_matrix = np.load(sim_hrf)
+    y = np.load(sim_data, allow_pickle=True)
+    hrf_matrix = np.load(sim_hrf, allow_pickle=True)
 
     # Run FISTA
     estimates, lambda_ = fista.fista(hrf_matrix, y, group=0.2)
 
     # Compare estimates
     assert lambda_ == 0.73388091
-    assert np.allclose(estimates, np.load(pylops_results), atol=1e-6)
+    assert np.allclose(estimates, np.load(pylops_results, allow_pickle=True), atol=1e-6)
