875e117ecc319f10e6cfafa951e87640dc82a9e6
Eneko Uruñuela <e.urunuela@icloud.com>
2022-09-29 15:45:41 +0200

    Use normal scaling for mad

--- pySPFM/deconvolution/select_lambda.py
+++ pySPFM/deconvolution/select_lambda.py
@@ -72,7 +72,7 @@ def select_lambda(hrf, y, criterion="ut", factor=1, pcg=0.7, lambda_echo=-1):
             y = y[(lambda_echo - 1) * nt : lambda_echo * nt, :]
 
     _, cD1 = wavedec(y, "db3", level=1, axis=0)
-    noise_estimate = median_abs_deviation(cD1) / 0.6745  # 0.8095
+    noise_estimate = median_abs_deviation(cD1, scale="normal") / 0.6745  # 0.8095
 
     if criterion == "mad":
         lambda_selection = noise_estimate
