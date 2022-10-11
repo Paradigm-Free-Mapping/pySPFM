43acbb47f088f0f7de6e2c5d776cefedee87052c
Eneko Uruñuela <e.urunuela@icloud.com>
2022-10-11 11:56:32 +0200

    Removed breakpoint

--- pySPFM/tests/test_hrf_generator.py
+++ pySPFM/tests/test_hrf_generator.py
@@ -17,6 +17,5 @@ def test_HRF_matrix(spm_single_echo, spm_single_echo_block, glover_multi_echo):
     te = np.array([18.4, 32.0, 48.4])
     hrf_object = hrf_generator.HRFMatrix(te=te, block=False, model="glover")
     hrf = hrf_object.generate_hrf(tr=1, n_scans=168).hrf_
-    breakpoint()
     hrf_loaded = np.load(glover_multi_echo)
     assert np.allclose(hrf, hrf_loaded)
