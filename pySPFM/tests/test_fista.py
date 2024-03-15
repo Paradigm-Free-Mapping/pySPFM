b8e40191cba1eeb7697f3fb987c0efbe0da2d1c3
Eneko Uruñuela <e.urunuela@icloud.com>
2024-03-15 14:44:05 +0000

    Added option to only use positive estimates in FISTA

--- pySPFM/tests/test_fista.py
+++ pySPFM/tests/test_fista.py
@@ -15,6 +15,22 @@ def test_fista(sim_data, sim_hrf, fista_results):
     assert np.allclose(estimates, np.load(fista_results, allow_pickle=True), atol=1e-6)
 
 
+def test_fista_positives(sim_data, sim_hrf, fista_results):
+    y = np.load(sim_data, allow_pickle=True)
+    hrf_matrix = np.load(sim_hrf, allow_pickle=True)
+
+    # Run FISTA without pylops
+    estimates, lambda_ = fista.fista(
+        hrf_matrix, y, group=0.2, use_pylops=False, max_iter=50, positive_only=True
+    )
+
+    # Compare estimates
+    assert np.allclose(lambda_, np.repeat(np.array([0.60157822]), y.shape[1], axis=0))
+    assert np.allclose(
+        estimates, np.maximum(np.load(fista_results, allow_pickle=True), 0), atol=1e-6
+    )
+
+
 def test_fista_pylops(sim_data, sim_hrf, pylops_results):
     y = np.load(sim_data, allow_pickle=True)
     hrf_matrix = np.load(sim_hrf, allow_pickle=True)
