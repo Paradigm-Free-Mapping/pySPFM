ef03260ada8131c3e7c3af774dcc1b549ec9cf0b
Eneko Uruñuela <e.urunuela@icloud.com>
2024-03-19 16:17:04 +0000

    Added __about__.py and edited init

---
+++ pySPFM/__about__.py
@@ -0,0 +1,20 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""Base module variables."""
+
+try:
+    from pySPFM._version import __version__
+except ImportError:
+    __version__ = "0+unknown"
+
+__packagename__ = "pySPFM"
+__copyright__ = "Copyright 2024, Paradigm Free Mapping"
+__credits__ = (
+    "Contributors: please check the ``.zenodo.json`` file at the top-level folder"
+    "of the repository"
+)
+__url__ = "https://github.com/Paradigm-Free-Mapping/pySPFM"
+
+DOWNLOAD_URL = (
+    f"https://github.com/Paradigm-Free-Mapping/{__packagename__}/archive/{__version__}.tar.gz"
+)
